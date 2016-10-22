8fb713b5266a8d69f4c2bf3708d752395915667e
lindsay <lindsayebangs@gmail.com>
2016-10-22 18:11:48 -0400

    Changed name of /home to /login.  Styled logout modal

--- sarah/www/js/app.js
+++ sarah/www/js/app.js
@@ -35,10 +35,10 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
   /* Sarah routes */
   .state('app.login', {
-    url: '/home',
+    url: '/login',
     views: {
       'menuContent': {
-        templateUrl: 'templates/home.html'
+        templateUrl: 'templates/login.html'
       }
     }
   })
@@ -125,5 +125,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
+  $urlRouterProvider.otherwise('/app/login');
 });
