4d5e39d0bd2046420e0ad27fcd942c09e8f19ab9
lindsay <lindsayebangs@gmail.com>
2016-10-22 11:59:16 -0400

    Added home page and updated titles in Reply Manager section

--- sarah/www/js/app.js
+++ sarah/www/js/app.js
@@ -34,6 +34,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   })
 
   /* Sarah routes */
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home.html'
+      }
+    }
+  })
+
   .state('app.analytics', {
     url: '/analytics',
     views: {
@@ -106,44 +115,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-
-  /* Default routes */
-  /*.state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  }); */
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/profile');
+  $urlRouterProvider.otherwise('/app/home');
 });
