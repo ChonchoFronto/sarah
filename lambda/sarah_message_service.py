25337c5b15f783d8b18b3e2c55a21a7788e6b0bb
JFC633 <jacob.learned@capitalone.com>
2016-10-23 00:54:39 -0400

    Updated Lambda functions

---
+++ lambda/sarah_message_service.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+import boto3
+from botocore.exceptions import ClientError
+import calendar
+import time
+
+class MessageService:
+
+    messages_table = boto3.resource('dynamodb').Table('SarahMessages')
+
+    def record_message(self, phone_number, message_received_from_user, message_sent_to_user):
+        item = {
+            "PhoneNumber": phone_number,
+            "Timestamp": -int(calendar.timegm(time.gmtime())),
+            "MessageReceived": message_received_from_user,
+            "MessageSent": message_sent_to_user
+        }
+
+        try:
+            response = self.messages_table.put_item(Item=item)
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            print("Message to " + phone_number + " saved to messages log")
+
+    
+    def get_message_log(self, limit):
+        response = self.messages_table.scan(Limit=limit)
+        
+        messages_list = [] if 'Items' not in response else response['Items']
+        messages_list = [{
+                            'PhoneNumber': m["PhoneNumber"],
+                            'Timestamp': time.strftime('%H:%M:%S %Y-%m-%d', time.localtime(-m['Timestamp'])),
+                            'MessageSent': m['MessageSent'],
+                            'MessageReceived': m['MessageReceived']
+                         } 
+                         for m in messages_list]
+        
+        print(messages_list)
+        return messages_list
