b5eaa26c897ef8fd7c94ec47ac670be7df72988e
JFC633 <jacob.learned@capitalone.com>
2016-10-23 02:28:14 -0400

    fixed some color and layout issues

--- sarah/www/js/app.js
+++ sarah/www/js/app.js
@@ -116,5 +116,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/login');
+  $urlRouterProvider.otherwise('/app/dashboard');
 });
