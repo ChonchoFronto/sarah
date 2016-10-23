7f34b46eb3c77f78bfc8ab14546ebdb8f9234d34
JFC633 <jacob.learned@capitalone.com>
2016-10-23 00:53:12 -0400

    Lambda functions for SARAH

--- lambda/sarah_command_service.py
+++ lambda/sarah_command_service.py
@@ -2,31 +2,61 @@ from __future__ import print_function
 import boto3
 import apiai
 import json
+from operator import itemgetter
 
 APIAI_CLIENT_ACCESS_TOKEN = "6acb38048ab042c68d6b2e73a4d7c5b8"
 
 class CommandService:
 
-    command_table = boto3.resource('dynamodb').Table('SarahSMSActions')
+    actions_table = boto3.resource('dynamodb').Table('SarahSMSActions')
 
-
-    def get_intent_from_apiai(self, message_body):
+    def get_response_message(self, message_body):
+        # Send message to api.ai for analysis
         ai = apiai.ApiAI(APIAI_CLIENT_ACCESS_TOKEN)
         request = ai.text_request()
         request.query = message_body
         
+        # Extract the recommended action from the response
         response = request.getresponse()
         json_response = json.loads(response.read())['result']
         action = json_response['action']
         print(response.read())
 
+        # Determine response message to send back to user
         if action in ["", "input.unknown"] or action.split('.')[0] == "smalltalk": 
             return json_response['fulfillment']['speech']
         else:
-            action_response = command_table.get_item(Key = {"Action": action})
+            action_response = self.actions_table.get_item(Key = {"Action": action})
             response_message = action_response['Item']['Response']
+
+            self.update_action_invocation_counts(action_response['Item'])
+
             return response_message
+
         
+    def update_action_invocation_counts(self, action_item):
+        item = {
+            "Action": action_item['Action'],
+            "Response": action_item['Response'],
+            "InvocationCount": 1 if 'InvocationCount' not in action_item else action_item['InvocationCount'] + 1
+        }
+
+        try:
+            response = self.actions_table.put_item(Item=item)
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            print("Counts for action " + action_item['Action'] + " updated.")
+        
+    
+    def get_actions_by_invocation_count(self):
+        response = self.actions_table.scan()
+        action_items = [] if 'Items' not in response else response['Items']
+        print(action_items)
+        action_items = sorted(action_items, key=itemgetter('InvocationCount'), reverse=True)
+        return action_items
+        
+
 
     
 
--- lambda/sarah_survivors.py
+++ lambda/sarah_survivors.py
@@ -1,23 +1,25 @@
 from __future__ import print_function
 import boto3
 from botocore.exceptions import ClientError
+from operator import itemgetter
 
 class Survivor:
     
     survivors_table = boto3.resource('dynamodb').Table('Survivors')
 
-    def __init__(self, phone_number):
+    def __init__(self, phone_number=None):
         # Construct from Dynamo if user exists, set properties for new user otherwise
-        try:
-            survivor_info = self.survivors_table.get_item(Key={ 'PhoneNumber': phone_number })
-        except ClientError as e:
-            print(e.response['Error']['Message'])  
-        else:
-            self.phone_number = phone_number
-            if 'Item' in survivor_info:
-                self.message_count = survivor_info['Item']['MessageCount']
+        if phone_number != None:
+            try:
+                survivor_info = self.survivors_table.get_item(Key={ 'PhoneNumber': phone_number })
+            except ClientError as e:
+                print(e.response['Error']['Message'])  
             else:
-                self.message_count = 0
+                self.phone_number = phone_number
+                if 'Item' in survivor_info:
+                    self.message_count = survivor_info['Item']['MessageCount']
+                else:
+                    self.message_count = 0
     
     
     def get_survivor_info(self):
@@ -34,3 +36,9 @@ class Survivor:
 
         response = self.survivors_table.put_item(Item=item)
         print('User ' + self.phone_number + ' updated.')
+
+    def get_survivors_by_messages_sent(self):
+        response = self.survivors_table.scan()
+        survivor_items = [] if 'Items' not in response else response['Items']
+        survivor_items.sort(key=itemgetter('MessageCount'), reverse=True)
+        return survivor_items
